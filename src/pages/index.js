16842c9d1eebece56c5830b04f6024e280f25d69
FaizulOsman <faizulosmanrabby@gmail.com>
2023-07-22 11:26:46 +0600

    feat: database connected

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ const HomePage = ({ allNews }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <DynamicBanner />
-      <AllNews allNews={data} />
+      <AllNews allNews={allNews} />
     </>
   );
 };
@@ -36,13 +36,13 @@ HomePage.getLayout = function getLayout(page) {
 };
 
 export const getServerSideProps = async () => {
-  const res = await fetch("http://localhost:5000/news");
+  const res = await fetch("http://localhost:3000/api/news");
   const data = await res.json();
   // console.log(data);
 
   return {
     props: {
-      allNews: data,
+      allNews: data?.data,
     },
     // revalidate: true,
   };
